9ba9a8a57bc9d11d1aa953b83ba203a75b9e4d92
sheeba-ansari <ansarisheeba5000@gmail.com>
2026-09-01 22:09:18 +0530

    Fix MySQL SSL certificate

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
+const fs = require("fs");
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: false
-    }
+   ssl: {
+    ca: fs.readFileSync("./ca.pem"),
+    rejectUnauthorized: true
+   }
 });
 db.connect(function(err) {
     if (err) {
