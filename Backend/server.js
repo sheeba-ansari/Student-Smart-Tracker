f1e1cd53f0cab7b2da388b24da10b07596dfccf1
sheeba-ansari <ansarisheeba5000@gmail.com>
2026-09-01 21:39:43 +0530

    Fix MySQL SSL connection

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const mysql = require("mysql2");
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     ssl: {
-        rejectUnauthorized: true
+        rejectUnauthorized: false
     }
 });
 db.connect(function(err) {
