57d3441797e0f759c13ebe6b42cbcf43bed3e3cd
sheeba-ansari <ansarisheeba5000@gmail.com>
2026-09-01 21:45:07 +0530

    Fix Aiven MySQL SSL

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
- const db = mysql.createConnection({
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
